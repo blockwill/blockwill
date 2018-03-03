53f68ab718b59f47826c60406fc1b718c4c5835e
David Terry <davidterry@posteo.de>
2018-03-03 20:05:12 +0100

    resolve merge conflict

--- contracts/Will.sol
+++ contracts/Will.sol
@@ -3,33 +3,33 @@ pragma solidity ^0.4.18;
 import './Destructible.sol';
 
 contract Will is Destructible {
-    
+
     uint public minValidators;
-    
+
     bool public isWillExecuted;
     uint public validatedCount;
     uint public transfers;
     uint[] public amountsTransfered;
-    
+
     struct Recipient {
         address recipient;
         uint8 percent;
     }
-    
+
     struct Validator {
         address validator;
         bool hasValidated;
     }
-    
+
     Recipient[] public recipients;
     Validator[] public validators;
-    
+
     event Validated(address);
     event Distributed();
     event Revoked();
-    
+
     function Will(address[] _recipients, uint8[] _percentages, address[] _validators, uint8 _minValidators) public {
-        
+
         uint8 totalPercentage;
         assert(_recipients.length == _percentages.length);
         recipients.length = _recipients.length;
@@ -37,35 +37,35 @@ contract Will is Destructible {
         for (uint i = 0; i < _recipients.length; i++) {
             address recipientAddress = _recipients[i];
             uint8 percent = _percentages[i];
-            
+
             assert(recipientAddress != 0x0);
             totalPercentage += percent;
-            
+
             recipients[i] = Recipient(recipientAddress, percent);
         }
-        
+
         assert(totalPercentage == 100);
         minValidators = _minValidators;
-        
+
         assert(minValidators <= _validators.length);
         validators.length = _validators.length;
-        
+
         for (uint j = 0; j < _validators.length; j++) {
             address validatorAddress = _validators[j];
             bool hasValidated = false;
-            
+
             validators[j] = Validator(validatorAddress, hasValidated);
         }
-        
+
     }
-    
+
     function validate() public {
         updateValidationState();
         if (countValidated() >= minValidators) {
             distributeFunds();
         }
     }
-    
+
     function updateValidationState() private {
         for (uint i = 0; i < validators.length; i++) {
             if (msg.sender == validators[i].validator) {
@@ -75,7 +75,7 @@ contract Will is Destructible {
             }
         }
     }
-    
+
     function countValidated() private returns (uint) {
         uint validated;
         for (uint i = 0; i < validators.length; i++) {
@@ -89,26 +89,26 @@ contract Will is Destructible {
 
     function distributeFunds() private {
         uint[] memory amounts = calculateAmounts();
-        
+
         for (uint i = 0; i < amounts.length; i++) {
             recipients[i].recipient.transfer(amounts[i]);
             transfers++;
-        }   
+        }
 
         isWillExecuted = true;
         Distributed();
     }
-       
+
     function calculateAmounts() private view returns (uint[]) {
         uint[] memory amounts = new uint[](recipients.length);
-        
+
         for (uint i = 0; i < recipients.length; i++) {
             amounts[i] = (this.balance * recipients[i].percent) / 100;
         }
-        
+
         return amounts;
     }
-    
+
     function() payable public {
     }
 
