cc89b217149ff44e564a6f598a46ed8f71531c32
David Terry <davidterry@posteo.de>
2018-03-03 13:19:07 +0100

    construction & funding

--- Will.sol
+++ Will.sol
@@ -5,29 +5,65 @@ import './Ownable.sol';
 contract Will is Ownable {
     
     uint private minValidators;
-    mapping(address => uint) private recipients;
-    mapping(address => bool) private validators;
+    
+    struct Recipient {
+        address recipient;
+        uint8 percent;
+    }
+    
+    struct Validator {
+        address validator;
+        bool hasValidated;
+    }
+    
+    Recipient[] recipients;
+    Validator[] validators;
     
     event Validated(address);
-    event Executed();
+    event Distributed();
     event Revoked();
-    event Funded();
     
-    function Will(address[] _recipients, uint[] _recipientPercentages, address[] _validators, uint _minValidators) public {
+    function Will(address[] _recipients, uint8[] _percentages, address[] _validators, uint8 _minValidators) public {
         
-        require(_recipients.length == _recipientPercentages.length);
+        uint8 totalPercentage;
+        assert(_recipients.length == _percentages.length);
+
+        recipients.length = _recipients.length;
+
         for (uint i = 0; i < _recipients.length; i++) {
-            recipients[_recipients[i]] = _recipientPercentages[i];
-            validators[_validators[i]] = false;
-            minValidators = _minValidators;
+            address recipientAddress = _recipients[i];
+            uint8 percent = _percentages[i];
+            
+            assert(recipientAddress != 0x0);
+            totalPercentage += percent;
+            
+            recipients[i] = Recipient(recipientAddress, percent);
+            // Recipient(recipientAddress, percent);
+        }
+        assert(totalPercentage == 100);
+        
+        minValidators = _minValidators;
+        assert(minValidators <= _validators.length);
+        
+        validators.length = _validators.length;
+        
+        for (uint j = 0; j < _validators.length; j++) {
+            address validatorAddress = _validators[j];
+            bool hasValidated = false;
+            
+            validators[j] = Validator(validatorAddress, hasValidated);
         }
         
     }
     
-    function validate() {}
-    function distributeFunds() {}
-    function revoke() onlyOwner {}
+    // function validate() {
+    //     msg.sender;
+    // }
     
-    function() payable public {}
+    // function distributeFunds() {}
+    // function revoke() onlyOwner {}
+    
+    function() payable public {
+    }
 
 }
